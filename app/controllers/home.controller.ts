b16f1c390b96dfbd6a861d035fd25a212d89e910
h0anguyen <hoadangnguyen11@gmail.com>
2024-09-23 17:39:05 +0700

    view, edit banner

--- app/controllers/home.controller.ts
+++ app/controllers/home.controller.ts
@@ -5,35 +5,43 @@ import { ApplicationController } from ".";
 
 export class HomeController extends ApplicationController {
   public async index(req: Request, res: Response) {
-
-      try {
-        let user = null;
-        if (req.session.userId) {
-          user = await prisma.user.findFirst({
-            where: {
-              id: req.session.userId,
-            },
-          });
-        }
-        const productsDatabase = await prisma.products.findMany();
-        const shuffledProducts = _.shuffle(productsDatabase);
-        const products = shuffledProducts.slice(0, 45);
-
-        const productsSold = await prisma.products.findMany({
-          orderBy: {
-            sold: "desc",
+    try {
+      let user = null;
+      if (req.session.userId) {
+        user = await prisma.user.findFirst({
+          where: {
+            id: req.session.userId,
           },
-          take: 100,
         });
-
-        res.render("userview/home.view/index", {
-          products,
-          user,
-          productsSold,
-        });
-      } catch (error) {
-        res.status(500).json({ message: "Đã xảy ra lỗi khi xử lý yêu cầu" });
       }
-    
+      const productsDatabase = await prisma.products.findMany();
+      const shuffledProducts = _.shuffle(productsDatabase);
+      const products = shuffledProducts.slice(0, 45);
+
+      const productsSold = await prisma.products.findMany({
+        orderBy: {
+          sold: "desc",
+        },
+        take: 100,
+      });
+      const banners = await prisma.images.findMany({
+        where: {
+          bannerId: {
+            not: null,
+          },
+        },
+        orderBy: {
+          bannerId: "asc",
+        },
+      });
+      res.render("userview/home.view/index", {
+        products,
+        user,
+        productsSold,
+        banners,
+      });
+    } catch (error) {
+      res.status(500).json({ message: "Đã xảy ra lỗi khi xử lý yêu cầu" });
+    }
   }
 }
